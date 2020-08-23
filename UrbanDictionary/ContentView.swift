804b71ef5ec29bf0ca7eb5402ab3c572a4ed8caf
Bram Schulting <bramschulting@me.com>
2020-08-23 16:06:30 +0200

    Initial Commit

---
+++ UrbanDictionary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UrbanDictionary
+//
+//  Created by Bram Schulting on 23/08/2020.
+//  Copyright © 2020 Bram Schulting. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
